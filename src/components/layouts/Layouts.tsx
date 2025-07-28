8ed58a663ecddeeb7d848dc5793e4161bf86e8af
kanghyun <dev.kanghyun@gmail.com>
2025-07-29 04:02:35 +0900

    header 폴더 명 변경

--- src/components/layouts/Layouts.tsx
+++ src/components/layouts/Layouts.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "@components/header-temp/Header";
+import Header from "@components/header/Header";
 import Footer from "@components/footer/Footer";
 import { StyledLayouts, StyledMain } from "./styles";
 
